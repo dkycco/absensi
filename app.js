2649685ab4bccc8ad3151aa68e8b594921fa2d36
dkycco <dikimuhamad525@gmail.com>
2025-07-20 21:29:11 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.use('/assets', express.static(__dirname + '/views/assets'))
+app.use('/layouts', express.static(__dirname + '/views/layouts'))
+
+app.get('/dashboard', (req, res) => {
+  res.render('pages/dashboard', {
+    layout: 'layouts/main-layout',
+  })
+})
+
+app.use('/', (req, res) => {
+    res.status(404)
+    res.send('<h1>404</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
