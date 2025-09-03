e75db739dcbce428cedfc84960f63668fa36b396
dkycco <dikimuhamad525@gmail.com>
2025-09-03 09:32:07 +0700

    Update

--- database/seeders/20250805075653_create_users_seeder.js
+++ database/seeders/20250805075653_create_users_seeder.js
@@ -6,11 +6,19 @@ const bcrypt = require('bcryptjs');
 module.exports = {
    up: async (queryInterface, Sequelize) => {
       const password = await bcrypt.hash('dikicyber@', 10);
+      const password2 = await bcrypt.hash('20208395', 10);
       await queryInterface.bulkInsert('users', [{
             nama_lengkap: 'Diki Muhamad Alfikri',
             username: 'dkycco',
             role: 'webmaster',
             password: password,
+            created_at: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+            updated_at: dayjs().format('YYYY-MM-DD HH:mm:ss')
+         },{
+            nama_lengkap: 'Admin',
+            username: 'admin',
+            role: 'admin',
+            password: password2,
             created_at: dayjs().format('YYYY-MM-DD HH:mm:ss'),
             updated_at: dayjs().format('YYYY-MM-DD HH:mm:ss')
          }
